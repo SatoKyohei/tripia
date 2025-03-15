8acef43a57b33c0672aeed4ceefc69f826014114
SatoKyohei <hkghsc3it@gmail.com>
2025-03-15 17:50:35 +0900

    update

--- frontend/src/components/elements/LocationSelect/LocationSelectGroups.tsx
+++ frontend/src/components/elements/LocationSelect/LocationSelectGroups.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "@mui/material";
 import BasicLocationSelect from "@/components/elements/LocationSelect/Basic/BasicLocationSelect";
+import { useAreaContext } from "@/contexts/AreaContext";
 
 // 課題：ラベルはここに定義で本当に良いのか
 const labels = {
@@ -10,22 +11,22 @@ const labels = {
 };
 
 // 課題：ダミーデータ
-const prefectures = [
-    { id: 1, name: "東京" },
-    { id: 2, name: "栃木" },
-    { id: 3, name: "群馬" },
-    { id: 4, name: "埼玉" },
-    { id: 5, name: "千葉" },
-    { id: 6, name: "神奈川" },
-];
-const areas = [
-    { id: 1, name: "新宿" },
-    { id: 2, name: "宇都宮" },
-    { id: 3, name: "前橋" },
-    { id: 4, name: "大宮" },
-    { id: 5, name: "舞浜" },
-    { id: 6, name: "横浜" },
-];
+// const prefectures = [
+//     { id: 1, name: "東京" },
+//     { id: 2, name: "栃木" },
+//     { id: 3, name: "群馬" },
+//     { id: 4, name: "埼玉" },
+//     { id: 5, name: "千葉" },
+//     { id: 6, name: "神奈川" },
+// ];
+// const areas = [
+//     { id: 1, name: "新宿" },
+//     { id: 2, name: "宇都宮" },
+//     { id: 3, name: "前橋" },
+//     { id: 4, name: "大宮" },
+//     { id: 5, name: "舞浜" },
+//     { id: 6, name: "横浜" },
+// ];
 
 type LocationSelectGroupsProps = {
     startPrefectureName?: string;
@@ -35,15 +36,33 @@ type LocationSelectGroupsProps = {
 };
 
 const LocationSelectGroups = (props: LocationSelectGroupsProps) => {
+    const { areaNames, prefectureNames } = useAreaContext();
+    
     return (
         <>
             <Stack direction="row" spacing={3}>
-                <BasicLocationSelect label={labels.departure} option={props.startPrefectureName} />
-                <BasicLocationSelect label={labels.departureArea} option={props.startAreaName} />
+                <BasicLocationSelect
+                    label={labels.departure}
+                    location={props.startPrefectureName}
+                    options={prefectureNames}
+                />
+                <BasicLocationSelect
+                    label={labels.departureArea}
+                    location={props.startAreaName}
+                    options={areaNames}
+                />
             </Stack>
             <Stack direction="row" spacing={3}>
-                <BasicLocationSelect label={labels.arrival} option={props.endPrefectureName} />
-                <BasicLocationSelect label={labels.arrivalArea} option={props.endAreaName} />
+                <BasicLocationSelect
+                    label={labels.arrival}
+                    location={props.endPrefectureName}
+                    options={prefectureNames}
+                />
+                <BasicLocationSelect
+                    label={labels.arrivalArea}
+                    location={props.endAreaName}
+                    options={areaNames}
+                />
             </Stack>
         </>
     );
--- frontend/src/libs/providers/Providers.tsx
+++ frontend/src/libs/providers/Providers.tsx
@@ -2,11 +2,16 @@
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 import { ReactNode } from "react";
+import { AreaProvider } from "@/contexts/AreaContext";
 
 // LocalizationProviderはクライアントサイドでしか動かない
 
 const Providers = ({ children }: { children: ReactNode }) => {
-    return <LocalizationProvider dateAdapter={AdapterMoment}>{children}</LocalizationProvider>;
+    return (
+        <LocalizationProvider dateAdapter={AdapterMoment}>
+            <AreaProvider>{children}</AreaProvider>
+        </LocalizationProvider>
+    );
 };
 
 export default Providers;
